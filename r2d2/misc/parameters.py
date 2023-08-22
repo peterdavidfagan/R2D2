098ad158500104d952e2e327d929dfcf9fcd2bdf
peterdavidfagan <peterdavidfagan@gmail.com>
2023-08-22 02:58:30 -0500

    add params

--- r2d2/misc/parameters.py
+++ r2d2/misc/parameters.py
@@ -2,13 +2,13 @@ import os
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = ""
-robot_ip = ""
-laptop_ip = ""
-gateway_ip = "" # only needed if connecting to internet via ethernet
-sudo_password = ""
-robot_type = ""  # 'panda' or 'fr3'
-robot_serial_number = ""
+nuc_ip = "192.168.1.10"
+robot_ip = "192.168.1.1"
+laptop_ip = "192.168.1.20"
+gateway_ip = "129.215.91.247" # only needed if connecting to internet via ethernet
+sudo_password = "robot"
+robot_type = "panda"  # 'panda' or 'fr3'
+robot_serial_number = "295341-1325422"
 # set libfranka version according to robot type
 if robot_type=='panda':
     libfranka_version = "0.9.0"
@@ -16,20 +16,19 @@ else:
     libfranka_version = "0.10.0"
 
 # Camera ID's #
-hand_camera_id = ""
-varied_camera_1_id = ""
-varied_camera_2_id = ""
+hand_camera_id = "head"
+varied_camera_1_id = "left"
+varied_camera_2_id = "right"
 
 # Charuco Board Params #
 CHARUCOBOARD_ROWCOUNT = 9
 CHARUCOBOARD_COLCOUNT = 14
 CHARUCOBOARD_CHECKER_SIZE = 0.020
-CHARUCOBOARD_MARKER_SIZE = 0.016
+CHARUCOBOARD_MARKER_SIZE = 0.015
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_100)
 
 # Ubuntu Pro Token (RT PATCH) #
-ubuntu_pro_token = ""
+ubuntu_pro_token = "C17CujhTteHnrdQW5wVEudVKA5cye"
 
 # Code Version [DONT CHANGE] #
 r2d2_version = "1.3"
-
