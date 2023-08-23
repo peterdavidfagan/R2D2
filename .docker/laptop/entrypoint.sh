dd176edf1484ea0a9edb0284050357e0dbc26ccf
peterdavidfagan <peterdavidfagan@gmail.com>
2023-08-23 10:12:20 +0100

    update conda activation for laptop script

--- .docker/laptop/entrypoint.sh
+++ .docker/laptop/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# activate conda
+source ~/miniconda3/bin/activate
+conda activate polymetis-local
+
 # install pyzed (TODO: move to main build after debug)
-cd /usr/local/zed && conda run -n "robot" python get_python_api.py
-conda run -n "robot" python -m pip install --ignore-installed /usr/local/zed/pyzed-4.0-cp37-cp37m-linux_x86_64.whl
+cd /usr/local/zed && python get_python_api.py
+python -m pip install --ignore-installed /usr/local/zed/pyzed-4.0-cp37-cp37m-linux_x86_64.whl
 
 # run user command
 exec "$@"
